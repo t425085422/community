38271e0559cb56d39f60836f7e4863df77dd4da1
root <425085422@qq.com>
2019-09-21 21:01:57 +0800

    'init'

---
+++ src/main/java/com/taotao/life/taotao/TaotaoApplication.java
@@ -0,0 +1,13 @@
+package com.taotao.life.taotao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaotaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaotaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/taotao/life/taotao/TaotaoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.taotao.life.taotao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaotaoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
