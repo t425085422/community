832174f125f96ca20f8d5613cbfbe7234b048081
root <425085422@qq.com>
2019-09-21 23:09:59 +0800

    '引入前台js文件'

--- src/main/java/com/taotao/life/taotao/controller/HelloController.java
+++ src/main/java/com/taotao/life/taotao/controller/HelloController.java
@@ -3,14 +3,10 @@ package com.taotao.life.taotao.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
-    @GetMapping("/greeting")
-    public String hello(@RequestParam(name = "name") String name, Model model){
-        model.addAttribute("name", name);
-        return "greeting";
-    }
+    @GetMapping("/")
+    public String index(){ return "index"; }
 }
